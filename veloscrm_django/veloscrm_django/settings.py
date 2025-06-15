c439186b901016cbaef52c1a466819b12776b07e
dominicmol <dominicmmol@gmail.com>
2025-06-15 20:21:08 +0200

    Revert settings.py to development config

--- veloscrm_django/veloscrm_django/settings.py
+++ veloscrm_django/veloscrm_django/settings.py
@@ -6,26 +6,39 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-wn_v7z89$xlu+@uyo@um7_0mzlbcc_m$i$6x5+(#tx)3j%%auy'
 
-# Zet DEBUG uit in productie
-DEBUG = False
+# Zet DEBUG aan voor development
+DEBUG = True
 
-# Sta alleen jouw PA-domein toe
-ALLOWED_HOSTS = [
-    'dominicmol.pythonanywhere.com',
-]
+# Tijdens development kan ALLOWED_HOSTS leeg blijven
+ALLOWED_HOSTS = []
 
-# Voor veilige CSRF-cookies bij production forms
-CSRF_TRUSTED_ORIGINS = [
-    'https://dominicmol.pythonanywhere.com',
-]
-
-# CORS alleen nodig voor je lokale frontend-dev
+# CORS alleen nodig als je lokaal de frontend op een andere poort draait
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:8080',
     'http://localhost:8081',
     'http://localhost:8082',
 ]
+CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -34,11 +47,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'corsheaders',
     'rest_framework',
     'rest_framework.authtoken',
     'djoser',
     'django_filters',
+
     'client.apps.ClientConfig',
     'lead.apps.LeadConfig',
     'team.apps.TeamConfig',
@@ -61,8 +76,7 @@ ROOT_URLCONF = 'veloscrm_django.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        # Voeg hier je Vue-build toe zodat index.html gevonden wordt
-        'DIRS': [ BASE_DIR / 'staticfiles' ],
+        'DIRS': [],  # álle views/template-mappen van je apps worden automatisch meegenomen
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -97,9 +111,8 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-# Statics
-STATIC_URL  = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+# Statische bestanden tijdens development
+STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
